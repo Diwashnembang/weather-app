28633387d442b39ea45100d3da14da3206ef61c6
Diwash Nembang <nembangd0@gmail.com>
2021-12-23 23:27:38 +0545

    fixed time

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 async function currentWeather() {
   try {
     const response = await fetch(
-      'https://api.openweathermap.org/data/2.5/weather?q=kathmandu&appid=78650a89fccd93f56814e9bef59802c3&fbclid=IwAR1gF3TXvgxoZgQqpoFFvRonBMUCvehGb1JoI5obR3Qqx8Lt1cLjPxwclcQ',
+      'https://api.openweathermap.org/data/2.5/weather?q=london&appid=78650a89fccd93f56814e9bef59802c3&fbclid=IwAR1gF3TXvgxoZgQqpoFFvRonBMUCvehGb1JoI5obR3Qqx8Lt1cLjPxwclcQ',
       {
         mode: 'cors',
         method: 'get',
@@ -16,15 +16,18 @@ async function currentWeather() {
 }
 const oneCall = async (lat, lon) => {
   try {
+    console.log(lat,lon)
     const apiCall = new Request(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=78650a89fccd93f56814e9bef59802c3`,
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=78650a89fccd93f56814e9bef59802c3`,
       {
         mode: 'cors',
         method: 'get',
       },
     );
-    const response = await apiCall;
+    const response = await fetch(apiCall);
+    console.log(response)
     const data = await response.json();
+    console.log(data)
     return data;
   } catch (err) {
     console.log(err);
--- src/main.js
+++ src/main.js
@@ -5,13 +5,22 @@ import './style.css';
 const moment = require('moment-timezone');
 
 async function main() {
-  const data = await currentWeather();
-  const city = renderText(data.name, '.city');
-  const description = renderText(data.weather[0].description, '.description');
-  const img = renderImage(data.weather[0].icon, '#image');
+  /* onecall api call gets all the the data for eg hourly and minutely weather
+  and is more precise which requires lat and lon that is why currentWeather
+  api is used to get the lat and lon the city .
+  */
+  const current = await currentWeather();
+  const oneCallData = await oneCall(current.coord.lat, current.coord.lon);
+  const city = renderText(current.name, '.city');
+  const description = renderText(oneCallData.current.weather[0].description, '.description');
+  const img = renderImage(oneCallData.current.weather[0].icon, '#image');
+
   // *utcoffset converts timezone into time
+  /* first moment.unix conver dt or epoch time in to date and tz into the time zone
+  */
+
   const date = renderText(
-    moment().utcOffset(data.timezone).utc().format('dddd, MMMM D YYYY, h:mm a'),
+    moment.unix(Number(oneCallData.minutely[0].dt)).utc().tz(oneCallData.timezone).format('dddd, MMMM D YYYY, h:mm a'),
     '.date',
   );
 }
